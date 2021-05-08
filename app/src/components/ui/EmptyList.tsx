abf6b84baf40fd97594b1881b1370bcc32659c43
Nik Graf <nik@nikgraf.com>
2021-05-08 18:38:21 +0200

    fix empty list styling

--- app/src/components/ui/EmptyList.tsx
+++ app/src/components/ui/EmptyList.tsx
@@ -2,19 +2,12 @@ import React, { ReactNode } from "react";
 import { StyleSheet, View } from "react-native";
 import { Icon } from "react-native-elements";
 import Spacer from "./Spacer";
-import { Dimensions } from "react-native";
-
-const width = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-  wrapper: {
-    bottom: "40%",
-    position: "absolute",
-    width,
-  },
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
   },
 });
 
@@ -25,12 +18,13 @@ type Props = {
 
 export default function EmptyList(props: Props) {
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.container}>
-        <Icon name={props.iconName} type="feather" color={"#000"} size={48} />
-        <Spacer />
-        {props.children}
-      </View>
+    <View style={styles.container}>
+      <Icon name={props.iconName} type="feather" color={"#000"} size={48} />
+      <Spacer />
+      {props.children}
+      {/* add two spacers to center it better vertically */}
+      <Spacer />
+      <Spacer />
     </View>
   );
 }

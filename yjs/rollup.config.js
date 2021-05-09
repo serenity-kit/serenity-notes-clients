d93603e4a3f943874000317681d657c5079c7908
Nik Graf <nik@nikgraf.com>
2021-05-09 22:10:51 +0200

    fix yjs vendor path

--- yjs/rollup.config.js
+++ yjs/rollup.config.js
@@ -4,10 +4,14 @@ import commonjs from "@rollup/plugin-commonjs";
 export default [
   {
     input: "./index.js",
-    output: { file: "../app/vendor/index.js", format: "cjs", sourcemap: true },
+    output: {
+      file: "../app/src/vendor/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
     plugins: [
       resolve({ mainFields: ["module", "browser", "main"] }),
-      commonjs()
-    ]
-  }
+      commonjs(),
+    ],
+  },
 ];

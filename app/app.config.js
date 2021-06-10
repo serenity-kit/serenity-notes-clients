4191f15418bd8c2fe72ac32e3f062a55a782a93c
Nik Graf <nik@nikgraf.com>
2021-06-10 23:06:32 +0200

    bump version

--- app/app.config.js
+++ app/app.config.js
@@ -4,7 +4,7 @@ export default {
     slug: "serenity-notes",
     privacy: "unlisted",
     platforms: ["ios", "android"],
-    version: "1.6.5",
+    version: "1.6.6",
     orientation: "portrait",
     icon: "./src/assets/icon.png",
     splash: {
@@ -24,7 +24,7 @@ export default {
     },
     android: {
       package: "re.serenity.notes",
-      versionCode: 16,
+      versionCode: 17,
       adaptiveIcon: {
         foregroundImage: "./src/assets/logo_serenity_android.png",
         backgroundColor: "#FFF",

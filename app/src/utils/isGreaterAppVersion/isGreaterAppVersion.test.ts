6a2d44718cb3d3bb96459d9f8917ff7f51b6c28d
Nik Graf <nik@nikgraf.com>
2021-07-08 07:57:22 +0200

    add more tests

--- app/src/utils/isGreaterAppVersion/isGreaterAppVersion.test.ts
+++ app/src/utils/isGreaterAppVersion/isGreaterAppVersion.test.ts
@@ -8,4 +8,7 @@ test("isGreaterAppVersion", () => {
   expect(isGreaterAppVersion("1.0.9", "1.0.0")).toBe(true);
   expect(isGreaterAppVersion("1.22.0", "1.0.0")).toBe(true);
   expect(isGreaterAppVersion("54.22.0", "22.0.0")).toBe(true);
+  expect(isGreaterAppVersion("54.22.0", "hello")).toBe(false);
+  expect(isGreaterAppVersion("wow", "1.0.0")).toBe(false);
+  expect(isGreaterAppVersion("1.0.0", "1.0")).toBe(false);
 });
